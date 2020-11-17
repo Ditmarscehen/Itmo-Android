3f33409d84912dd7eaf5071f6d18d8649aacb38a
Dmitry <284629@niuitmo.ru>
2020-11-18 00:33:08 +0300

    added post

--- fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,55 +1,80 @@
 package com.example.fragments
 
+
 import android.os.Bundle
-import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var homeFragment: Fragment
-    private lateinit var dashboardFragment: Fragment
-    private lateinit var notificationsFragment: Fragment
-    val fragmentManager: FragmentManager = supportFragmentManager
-    var active: Fragment = homeFragment
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
-        val navController = host.navController
-        setUpBottomNav(navController)
-        val bottomNavigationView:BottomNavigationView=bottom_nav_view
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        if (savedInstanceState == null) {
+            setupBottomNavigation()
+        }
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigation()
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
     }
 
-    private val mOnNavigationItemSelectedListener: BottomNavigationView.OnNavigationItemSelectedListener =
-        object : BottomNavigationView.OnNavigationItemSelectedListener {
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when (item.itemId) {
-                    R.id.home_fragment ->{
+    private fun setupBottomNavigation() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        val navGraphIds = listOf(
+            R.navigation.home_navigation,
+            R.navigation.dashboard_navigation,
+            R.navigation.notifications_navigation
+        )
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.my_nav_host_fragment,
+            intent = intent
+        )
 
+
+        currentNavController = controller
+    }
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            var a:Int= 1
+            val currentTag = bottom_nav_view.selectedItemId
+            Toast.makeText(applicationContext,"$currentTag ${R.id.dashboard_navigation}",Toast.LENGTH_SHORT).show()
+            val currentRoot = supportFragmentManager.findFragmentById(a)
+            if (currentRoot != null) {
+                Toast.makeText(applicationContext,"sasai",Toast.LENGTH_SHORT).show()
+                currentRoot.childFragmentManager.popBackStackImmediate()
+                if (currentRoot.childFragmentManager.backStackEntryCount == 0) {
+                    currentRoot.fragmentManager?.popBackStackImmediate(
+                        currentTag,
+                        FragmentManager.POP_BACK_STACK_INCLUSIVE
+                    )
+                    val lastTag = currentTag-1
+                    if (lastTag != 0 ) {
+                        bottom_nav_view.selectedItemId = lastTag
+                    } else {
+                        finish()
                     }
                 }
-                return false
+            }else{
+                Toast.makeText(applicationContext,"x_x",Toast.LENGTH_SHORT).show()
             }
         }
-
-    private fun setUpBottomNav(navController: NavController) {
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
-        bottomNav?.setupWithNavController(navController)
     }
 
-    enum class Tags {
-        HOME, DASHBOARD, NOTIFICATIONS
-    }
 
-    
+
 }
 
